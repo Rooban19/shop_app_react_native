fb560dd1d2d0f1d74b8b0e4d3a9a20b57c2c4632
ROOBAN <roobandc8@gmail.com>
2020-09-21 10:10:11 +0530

    Order Screen rendred with styles and show details button working..(Upto building User Product Screen

--- screens/shop/CartScreen.js
+++ screens/shop/CartScreen.js
@@ -51,6 +51,7 @@ const CartScreen = props => {
             quantity={itemData.item.quantity}
             title={itemData.item.productTitle}
             amount={itemData.item.sum}
+            deletable
             onRemove={() => {
               dispatch(cartAction.removeFromCart(itemData.item.productId));
             }}
--- screens/shop/OrderScreen.js
+++ screens/shop/OrderScreen.js
@@ -2,20 +2,28 @@ import React from 'react';
 import { FlatList, Text } from 'react-native';
 import { useSelector } from 'react-redux';
 import { HeaderButtons, Item } from 'react-navigation-header-buttons';
+import OrderItem from '../../components/shop/OrderItem';
 
 import HeaderButton from '../../components/UI/HeaderButton';
 
 const OrderScreen = props => {
   const orders = useSelector(state => state.order.orders);
   console.log('ORDERSSS', orders);
+  console.log('DATE', orders.date);
   return (
     <FlatList
       data={orders}
       keyExtractor={item => item.id}
-      renderItem={itemData => <Text>{itemData.item.totalAmount}</Text>}
+      renderItem={itemData => (
+        <OrderItem
+          amount={itemData.item.totalAmount}
+          date={itemData.item.readableDate}
+          items={itemData.item.items}
+        />
+      )}
     />
   );
-};
+  };
 
 OrderScreen.navigationOptions = navData => {
   return {
